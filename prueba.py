a6588af5b35dd458d78f2151215e81fc50728bc3
Jesús Márquez <jesusmarquezdelgado@gmail.com>
2020-08-20 19:07:26 +0200

    ALL

--- prueba.py
+++ prueba.py
@@ -14,11 +14,19 @@ while(True):
         time.sleep(1)
 
         f = open(path+"/"+files[0],"r")
-
         strings = f.read().split(" ",1)
 
-        webbrowser.get(browser_path).open("http://"+strings[0]+".com")
+        if(strings[0] == "bloquear"):
+            os.system("rundll32.exe user32.dll, LockWorkStation")
 
-        f.close()
+        elif(strings[0] == "apagar"):
+            os.system("shutdown /s")
+
+        elif(strings[0] == "reiniciar"):
+            os.system("shutdown /r")
+
+        else:
+            webbrowser.get(browser_path).open("http://"+strings[0]+".com")
+            f.close()
 
         os.remove(path+"/"+files[0])
