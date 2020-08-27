114251ab35650a2990f6646c5433fd42e801172b
Kaiser16 <jesusmarquezdelgado@gmail.com>
2020-08-27 03:20:46 +0200

    add

--- prueba.py
+++ prueba.py
@@ -1,17 +1,4 @@
+import webbrowser
 
-import dropbox
-import hashlib
-
-accessToken = 'sl.AgOGDMe1Va54wk37OzuQCndtndRjUtzeiRSzckFAJZ_LMMfse0TsZB8ZcsLHK_-2faqjdAW-Unrie2qNwdEhaZq7-JLaSQkk9Qt0tcvPoIuM-Tdssz6CU8JwzuC1WNaPFzYSZec'
-folder = "/Amazon Alexa"
-dbx = dropbox.Dropbox(accessToken)
-response = dbx.files_list_folder(path=folder)
-
-
-
-
-
-#for f in response.entries:
-path = folder+"/"+response.entries[0].name
-md,res = dbx.files_download(path)
-dbx.files_delete_v2(path)
+query_string = "naruto"
+webbrowser.open("https://www.youtube.com/results?search_query=" + query_string)
